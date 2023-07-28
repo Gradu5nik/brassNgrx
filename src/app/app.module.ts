e0ab2937188fb855d743148420481149234b24df
Georgijs Katasevs <gkatasev@gmail.com>
2023-07-28 11:44:50 +0300

    devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge:10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
